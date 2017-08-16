eab75ecbd0b79720ec08e3435ea2843e099dd932
warrickpk <warrickpk@gmail.com>
2017-08-16 22:08:52 +0100

    Initial commit Loading data files

---
+++ intro-to-data-science/hypothesis-testing/hypothesis_testing.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National',
+          'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana',
+          'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho',
+          'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan',
+          'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico',
+          'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa',
+          'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana',
+          'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California',
+          'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island',
+          'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia',
+          'ND': 'North Dakota', 'VA': 'Virginia'}
+
+def get_list_of_university_towns():
+
+    return "ANSWER"
+
+uni_towns = pd.read_table('./data/university_towns.txt')
+print(uni_towns)
+
+
+#gdp = pd.read_excel('./data/gdplev.xls')#,skiprows=17,header=0)
+#house_prices = pd.read_csv('./data/City_Zhvi_AllHomes.csv')
+#GDP = pd.read_csv('world_bank.csv',skiprows=4)
